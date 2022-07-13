97fb83e8bca16fc915778971c1ee1cb71002e80c
codenoob1997 <h1013121231@live.com>
2022-07-13 13:30:27 +0800

    新增了一些组件

---
+++ src/components/Doodles/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-dropdown
+    trigger="click"
+    class="international"
+    @command="handleDoodle"
+  >
+    <div class="button">
+          <img src="@/assets/images/doodles-text-color.png" alt="doodlepic">
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>
+          Introduction
+        </el-dropdown-item>
+        <el-dropdown-item>
+          SpaceDoodles
+        </el-dropdown-item>
+        <el-dropdown-item>
+          Doodles2
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import {} from 'vue'
+const handleDoodle= function(){
+    alert("doodles")
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+  img{
+    width: 162px;
+    height: 36px;
+  }
+
+</style>
---
+++ src/components/discord/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-dropdown
+    trigger="click"
+    class="international"
+    @command="handleDoodle"
+  >
+    <div class="button">
+          <div class="img">
+            discord
+          </div>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>
+          Doodles Official
+        </el-dropdown-item>
+        <el-dropdown-item>
+          Doodles China Community 
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import {} from 'vue'
+const handleDoobros= function(){
+    alert("doobros")
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+  .img{
+    width: 162px;
+    height: 36px;
+    text-align: center;
+  }
+
+</style>
